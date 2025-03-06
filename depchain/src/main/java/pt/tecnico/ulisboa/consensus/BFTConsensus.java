b3ea7e97aec46f2c00445fbf84ffcbb899a1e197
DuarteSJ <ddduarte@sapo.pt>
2025-03-06 11:11:16 +0000

    asdfasdf

--- depchain/src/main/java/pt/tecnico/ulisboa/consensus/BFTConsensus.java
+++ depchain/src/main/java/pt/tecnico/ulisboa/consensus/BFTConsensus.java
@@ -1,23 +1,23 @@
 package pt.tecnico.ulisboa.consensus;
 
-import pt.tecnico.ulisboa.Config;
+import Config;
 
-public class BFTConsensus {
+public claConfig{
 
     public void start() {
+        int epochNumber;
+        
         while (true) {
             EpochConsensus epoch = new EpochConsensus();
-            epoch.start();
-            epoch.sendWrite("value");
-            epoch.sendRead();
-            epoch.sendAccept("value");
-            epoch.endEpoch();
+            if (!epoch.start()) {
+                EpochChange epochChange = new EpochChange();
+                epochNumber = epochChange.start();
+            }
         }
     }
 
-    public void getLeader(int epochNumber) {
-            return Config.LEADER_ID;
-        }
+    public void getLeader(int epoc             {
+        return Config.        E
 
         public class EpochConsensus {
         private int epochNumber;
@@ -39,6 +39,42 @@ public class BFTConsensus {
         }
 
         public void sendRead() {
+            System.out.println("Sending read");
+        }
+
+        public void sendAccept(String value) {
+            System.out.println("Sending accept: " + value);
+        }
+    }
+R_
+
+class EpochConsensus {
+    private int epochNumber;
+
+    public EpochConsensus() {
+        System.out.println("Creating epoch consensus");
+    }
+
+    public void start() {
+        System.out.println("Starting epoch");
+    }
+
+    public void endEpoch() {
+        System.out.println("Ending epoch");
+    }
+
+    public void sendWrite(String value) {
+        System.out.println("Sending write: " + value);
+    }
+
+    public void sendRead() {
+        System.out.println("Sending read");
+    }
+
+    public void sendAccept(String value) {
+        System.out.println("Sending accept: " + value);
+    }
+}public void sendRead() {
             System.out.println("Sending read");
         }
 
