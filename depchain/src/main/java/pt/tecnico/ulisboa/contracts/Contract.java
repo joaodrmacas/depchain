6aef5565e149863b84dbecf194d0fd16d0123e57
João Maçãs <joaomacas02@gmail.com>
2025-03-27 01:33:09 +0000

    added: blackList interface and test

--- depchain/src/main/java/pt/tecnico/ulisboa/contracts/Contract.java
+++ depchain/src/main/java/pt/tecnico/ulisboa/contracts/Contract.java
@@ -4,17 +4,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Map;
 
+import pt.tecnico.ulisboa.utils.types.Logger;
 import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.evm.EvmSpecVersion;
 import org.hyperledger.besu.evm.fluent.EVMExecutor;
 import org.hyperledger.besu.evm.fluent.SimpleWorld;
 import org.hyperledger.besu.evm.tracing.StandardJsonTracer;
 import org.hyperledger.besu.datatypes.Wei;
 import org.apache.tuweni.bytes.Bytes;
 
+
 import pt.tecnico.ulisboa.utils.ContractUtils;
 
 public abstract class Contract {
-    // private Bytes BYTECODE;
     
     protected static Map<String, String> METHOD_SIGNATURES;
 
@@ -24,7 +26,7 @@ public abstract class Contract {
     protected final StandardJsonTracer tracer;
     protected final SimpleWorld world;
 
-    public Contract(SimpleWorld world, Address owner) {        
+    public Contract(SimpleWorld world, Address owner, String byteCode, String deployCode) {        
         // Generate contract address
         this.contractAddress = ContractUtils.generateContractAddress(owner);
         this.world = world;
@@ -34,21 +36,24 @@ public abstract class Contract {
 
         //Create objects to catch output
         //TODO: o tracer é para dar memory state,stack e de gas, pode ser util mas a partida nao é preciso
+        this.executor = EVMExecutor.evm(EvmSpecVersion.CANCUN);
         this.output = new ByteArrayOutputStream();
         this.tracer = new StandardJsonTracer(new PrintStream(output), true, true, true, true);
 
-
-        this.executor = EVMExecutor.evm();
         executor.tracer(tracer);
         executor.sender(owner); //TODO: assuming there's only one admin account
         executor.receiver(contractAddress);
-
     }
 
-    public void setByteCode(String bytecode) {
-        Bytes b = Bytes.fromHexString(bytecode);
-        // this.BYTECODE = b;
+    public void setByteCode(String byteCode) {
+        Bytes b = Bytes.fromHexString(byteCode);
         executor.code(b);
     }
+
+    public Address getAddress(){
+        return contractAddress;
+    }
+
+    public abstract void deploy(Object... args);
 
 }
--- depchain/src/main/java/pt/tecnico/ulisboa/utils/ContractUtils.java
+++ depchain/src/main/java/pt/tecnico/ulisboa/utils/ContractUtils.java
@@ -7,7 +7,6 @@ import org.hyperledger.besu.datatypes.Address;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.crypto.Hash;
 
-import pt.tecnico.ulisboa.utils.types.Logger;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -15,13 +14,37 @@ import com.google.gson.JsonParser;
 
 public class ContractUtils {
     
-    public static String padAddressTo256Bit(String hexAddress) {
-        return String.format("%064x", new BigInteger(hexAddress.substring(2), 16));
-    }
 
     public static String padBigIntegerTo256Bit(BigInteger value) {
         return String.format("%064x", value);
     }
+
+    public static String padAddressTo256Bit(Address addr) {
+        String addrHex = addr.toHexString().substring(2); // Remove '0x'
+        return "0".repeat(64 - addrHex.length()) + addrHex;
+    }
+
+    public static String padHexStringTo256Bit(String hexString) {
+        if (hexString.startsWith("0x")) {
+            hexString = hexString.substring(2);
+        }
+
+        int length = hexString.length();
+        int targetLength = 64;
+
+        if (length >= targetLength) {
+            return hexString.substring(0, targetLength);
+        }
+
+        return "0".repeat(targetLength - length) +
+                hexString;
+    }
+
+    public static String convertIntegerToHex256Bit(int number) {
+        BigInteger bigInt = BigInteger.valueOf(number);
+        return String.format("%064x", bigInt);
+    }
+
     
     public static Address generateContractAddress(Address deployerAddress) {
         return Address.fromHexString(
@@ -51,94 +74,42 @@ public class ContractUtils {
         return Integer.decode("0x"+returnData);
     }
 
-    //TODO: test this shit
     public static boolean extractBooleanFromReturnData(ByteArrayOutputStream byteArrayOutputStream) {
-        try {
-            String[] lines = byteArrayOutputStream.toString().split("\\r?\\n");
-            
-            // Ensure we have at least one line of JSON output
-            if (lines.length == 0) {
-                Logger.LOG("No JSON output found");
-                return false;
-            }
-            
-            // Parse the last line of JSON
-            JsonObject jsonObject = JsonParser.parseString(lines[lines.length - 1]).getAsJsonObject();
-            
-            // Validate presence of required JSON fields
-            if (!jsonObject.has("memory") || !jsonObject.has("stack")) {
-                Logger.LOG("Missing memory or stack in JSON output");
-                return false;
-            }
-            
-            String memory = jsonObject.get("memory").getAsString();
-            JsonArray stack = jsonObject.get("stack").getAsJsonArray();
-            
-            // Validate stack has sufficient elements
-            if (stack.size() < 2) {
-                Logger.LOG("Insufficient stack elements");
-                return false;
-            }
-            
-            // Parse offset and size from stack
-            int offset, size;
-            try {
-                offset = Integer.decode(stack.get(stack.size() - 1).getAsString());
-                size = Integer.decode(stack.get(stack.size() - 2).getAsString());
-            } catch (NumberFormatException e) {
-                Logger.LOG("Failed to parse stack elements: " + e.getMessage());
-                return false;
-            }
-            
-            // Validate memory substring access
-            int memoryStartIndex = 2 + offset * 2;
-            int memoryEndIndex = memoryStartIndex + size * 2;
-            
-            if (memoryStartIndex < 0 || memoryEndIndex > memory.length()) {
-                Logger.LOG("Invalid memory substring indices");
-                return false;
-            }
-            
-            // Extract return data
-            String returnData = memory.substring(memoryStartIndex, memoryEndIndex);
-            Logger.LOG("RETURN DATA: " + returnData);
-            
-            // In Solidity, booleans are typically the least significant byte (0x00 for false, 0x01 for true)
-            // Trim any leading zeros and check the last byte
-            returnData = returnData.replaceFirst("^0+", "");
-            
-            // Return true if last byte is non-zero
-            return !returnData.equals("00") && !returnData.isEmpty();
-            
-        } catch (Exception e) {
-            Logger.LOG("Error extracting boolean: " + e.getMessage());
-            return false;
-        }
-    }
+        String[] lines = byteArrayOutputStream.toString().split("\\r?\\n");
+        JsonObject jsonObject = JsonParser.parseString(lines[lines.length - 1]).getAsJsonObject();
     
-    //TODO: test this shit
+        String memory = jsonObject.get("memory").getAsString().substring(2); // Remove '0x'
+    
+        JsonArray stack = jsonObject.get("stack").getAsJsonArray();
+        int offset = Integer.decode(stack.get(stack.size() - 1).getAsString());
+        int size = Integer.decode(stack.get(stack.size() - 2).getAsString());
+    
+        // Extract the relevant portion of memory
+        String returnData = memory.substring(offset * 2, offset * 2 + size * 2);
+        
+        // Remove leading zeros
+        returnData = returnData.replaceFirst("^0+", "");
+        
+        // Convert to boolean (any non-zero value is true)
+        return !returnData.equals("0") && !returnData.isEmpty();
+    }
+
     public static String extractStringFromReturnData(ByteArrayOutputStream byteArrayOutputStream) {
         String[] lines = byteArrayOutputStream.toString().split("\\r?\\n");
         JsonObject jsonObject = JsonParser.parseString(lines[lines.length - 1]).getAsJsonObject();
     
-        String memory = jsonObject.get("memory").getAsString();
+        String memory = jsonObject.get("memory").getAsString().substring(2); // Remove '0x'
     
         JsonArray stack = jsonObject.get("stack").getAsJsonArray();
         int offset = Integer.decode(stack.get(stack.size() - 1).getAsString());
+        int size = Integer.decode(stack.get(stack.size() - 2).getAsString());
     
-        // First word (32 bytes) contains the length of the string
-        int stringLength = Integer.decode("0x" + memory.substring(2 + offset * 2, 2 + offset * 2 + 64));
+        // Extract the relevant portion of memory
+        String returnData = memory.substring(offset * 2, offset * 2 + size * 2);
         
-        // Actual string data starts after the length word
-        String stringHex = memory.substring(2 + offset * 2 + 64, 2 + offset * 2 + 64 + stringLength * 2);
+        // Remove leading zeros
+        returnData = returnData.replaceFirst("^0+", "");
         
-        // Convert hex to string
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < stringHex.length(); i += 2) {
-            String str = stringHex.substring(i, i + 2);
-            output.append((char) Integer.parseInt(str, 16));
-        }
-        
-        return output.toString();
+        return returnData;
     }
 }
